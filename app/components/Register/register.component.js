c9c29bd408df211162880222bdb4f076924ce642
Donald Sisco <dododonnars@gmail.com>
2017-05-03 09:21:45 -0400

    Fixed register email input

--- app/components/Register/register.component.js
+++ app/components/Register/register.component.js
@@ -6,7 +6,7 @@ class Register extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            emai: "",
+            email: "",
             username: "",
             password: "",
             confirmationPassword: "",
