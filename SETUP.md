5ba153edba00d5861f2569fc7ffd2fcd4fd4c252
pastorsj <pastorsj@rose-hulman.edu>
2017-05-10 17:40:28 -0400

    Added production url

---
+++ SETUP.md
@@ -0,0 +1,9 @@
+## Urls
+Local development
+```
+http://localhost:8080
+```
+Production Development
+```
+http://ec2-34-224-40-124.compute-1.amazonaws.com:8080
+```
--- app/components/constants.js
+++ app/components/constants.js
@@ -1,4 +1,5 @@
 const API_URL = "http://localhost:8080";
+// const API_URL = "http://ec2-34-224-40-124.compute-1.amazonaws.com:8080";
 
 export default {
     API_URL
